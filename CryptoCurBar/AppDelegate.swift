b7fd05a99a46fed662f24caa46028f81b50345dc
Gerben de Graaf <gerb@gerbens-imac-5.local>
2018-01-13 14:29:28 +0100

    Initial Commit

---
+++ CryptoCurBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CryptoCurBar
+//
+//  Created by Gerben de Graaf on 13/01/2018.
+//  Copyright © 2018 Gerben de Graaf. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
